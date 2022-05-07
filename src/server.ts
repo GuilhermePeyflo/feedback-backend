bcb9223a67ef96082aa0b592eb35ac8749c95426
Peyflo <guilherme.peyflo@gmail.com>
2022-05-07 01:36:36 -0300

    adjusting port in production

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ app.use(cors())
 app.use(express.json())
 app.use(route)
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log(`Server started\nhttp://localhost:8000`)
 })
